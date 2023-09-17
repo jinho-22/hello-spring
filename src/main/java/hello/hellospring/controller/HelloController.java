0322a4c41df471ca506294c3bd0b8d82f0f7544c
jinho-22 <dn70032615@gmail.com>
2023-09-17 23:05:58 +0900

    2023/09/17

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,4 +21,11 @@ public class HelloController {
         return "hello-template";
     }
 //    http://localhost:8080/hello-mvc?name=Spring!!!
+
+
+    @GetMapping("hello-string")
+    @ResponseBody //http body부분에 아래데이터를 직접넣어준다는 내용
+    public String helloString(@RequestParam("name ") String name){
+        return "hello" + name;
+    }
 }
