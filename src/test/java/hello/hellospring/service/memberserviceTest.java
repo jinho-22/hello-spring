ac53f3af108a8334887ccb4409869e11d76d0b6d
jinho-22 <dn70032615@gmail.com>
2023-10-08 15:16:42 +0900

    2023/10/08 command+shift+T 로 아~~~~~ 주 쉽게 테스트 생성함^^

---
+++ src/test/java/hello/hellospring/service/memberserviceTest.java
@@ -0,0 +1,20 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class memberserviceTest {
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
